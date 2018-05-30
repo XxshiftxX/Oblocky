ef94259611a3a4df965da6d1d1d15265ad3abe3d
Unknown <qjfrntop12@naver.com>
2018-05-30 23:41:02 +0900

    조건문 추가
    - IfBlock, ElseIfBlock, ElseBlock 추가

---
+++ Oblocky/ElseBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oblocky
+{
+    class ElseBlock : IBlock
+    {
+        public IBlock NextBlock { get; set; }
+        public IBlock InnerBlock { get; set; }
+
+        public void Execute()
+        {
+            InnerBlock?.Execute();
+            NextBlock?.Execute();
+        }
+    }
+}
---
+++ Oblocky/IfBlock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oblocky
+{
+    class IfBlock : IBlock
+    {
+        public IBlock NextBlock { get; set; }
+        public IBlock InnerBlock { get; set; }
+
+        public bool Condition { get; set; }
+
+        public void Execute()
+        {
+            var condition = Condition;
+            if(condition)
+            {
+                InnerBlock?.Execute();
+
+                var next = NextBlock ?? null;
+                
+                while (next is ElseBlock || next is ElseIfBlock)
+                    next = next?.NextBlock;
+
+                next?.Execute();
+            }
+            else
+            {
+                NextBlock?.Execute();
+            }
+        }
+    }
+}
